5e3bdab60eda4ba8d55d9a116576e33766fb3c62
Evaldas Vaitonis <ritmas@Evaldass-iMac.local>
2019-05-08 19:00:07 +0300

    register form updated

--- src/Form/RouteRegisterType.php
+++ src/Form/RouteRegisterType.php
@@ -2,21 +2,31 @@
 
 namespace App\Form;
 
-use App\Entity\Route;
-use App\Entity\WorkShift;
-use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
 use Symfony\Component\Form\Extension\Core\Type\HiddenType;
+use Symfony\Component\Form\Extension\Core\Type\NumberType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
-use Symfony\Component\OptionsResolver\OptionsResolver;
+
 
 class RouteRegisterType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
+            ->add('name', TextType::class, ['label' => 'Vardas'])
+            ->add('age', NumberType::class, ['label' => 'Amžius'])
+            ->add('role', ChoiceType::class, [
+               'choices' => [
+                   'Vairuotojas' => 'driver',
+                   'Keleivis' => 'client',
+               ],
+                'label' => 'Profilio tipas'
+            ])
+            ->add('route_description', TextareaType::class)
             ->add('origin-input',TextType::class, [
                 'attr' => ['class' => 'some-Css-Class'],
             ])
@@ -25,10 +35,7 @@ class RouteRegisterType extends AbstractType
             ])
             ->add('home_location',HiddenType::class )
             ->add('work_location',HiddenType::class)
-            ->add('work_shift_id',EntityType::class, [
-                'class' => WorkShift::class,
-                'choice_label' => 'name',
-            ])
+            ->add('description', TextareaType::class)
             ->add('save', SubmitType::class, ['label' => 'Saugoti'])
         ;
     }
