904c2f9bf6c0be2058bc6f6907cf26d2633bc12e
Docker fake user <docker.fake.user@example.com>
2019-05-20 09:03:58 +0000

    migrations fix

--- src/Migrations/Version20190520090344.php
+++ src/Migrations/Version20190520090344.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20190511092935 extends AbstractMigration
+final class Version20190520090344 extends AbstractMigration
 {
     public function getDescription() : string
     {
@@ -22,7 +22,6 @@ final class Version20190511092935 extends AbstractMigration
         // this up() migration is auto-generated, please modify it to your needs
         $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
 
-        $this->addSql('DROP TABLE matcher');
         $this->addSql('ALTER TABLE user CHANGE age age INT DEFAULT NULL');
     }
 
@@ -31,7 +30,6 @@ final class Version20190511092935 extends AbstractMigration
         // this down() migration is auto-generated, please modify it to your needs
         $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
 
-        $this->addSql('CREATE TABLE matcher (id INT AUTO_INCREMENT NOT NULL, driver_id INT NOT NULL, client_id INT NOT NULL, work_distance INT NOT NULL, home_distance INT NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8 COLLATE utf8_unicode_ci ENGINE = InnoDB COMMENT = \'\' ');
         $this->addSql('ALTER TABLE user CHANGE age age INT DEFAULT NULL');
     }
 }
