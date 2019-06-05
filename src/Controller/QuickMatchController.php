8902764de10642bc552da89e1f0ac66ddfc534d3
Evaldas Vaitonis <ritmas@Evaldass-iMac.local>
2019-06-05 15:47:12 +0300

    quick search and matches

--- src/Controller/QuickMatchController.php
+++ src/Controller/QuickMatchController.php
@@ -2,11 +2,10 @@
 
 namespace App\Controller;
 
-use App\Entity\User;
-use App\Entity\Route as UserRoute;
 use App\Form\MatchFilterType;
 use App\Service\MatchMaker;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -20,12 +19,14 @@ class QuickMatchController extends AbstractController
      */
     public function index(Request $request, MatchMaker $matchMaker)
     {
-        $user = new User();
-        $route = new UserRoute();
-        $route->setHomeLocation('EihQaWzEl27FsyBnYXR2xJcgMTUsIFJpbmdhdWRhaSwgTGl0aHVhbmlhIjASLgoUChIJK5sfjKQh50YRdGMir1MkEgMQDyoUChIJD16-vqMh50YRt7rzJ9j7cr8');
-        $route->setWorkLocation('EiRCcmFzdG9zIGdhdHbElyAxNCwgS2F1bmFzLCBMaXRodWFuaWEiMBIuChQKEglR0T6lBiLnRhF7DAxS4r2SaBAOKhQKEgn52adUACLnRhEhTzFKDJUW6w');
-        $user->setRoute($route);
-        $user->setRole('client');
+        if ($this->get('security.authorization_checker')->isGranted('IS_AUTHENTICATED_FULLY')) {
+            return $this->redirectToRoute('homepage');
+        }
+        if($this->get('session')->get('user')) {
+            $user = $this->get('session')->get('user');
+        } else {
+            return new RedirectResponse($this->generateUrl('quick_search'));
+        }
         $form = $this->createForm(MatchFilterType::class);
         $form->handleRequest($request);
 
--- src/Controller/QuickSearchController.php
+++ src/Controller/QuickSearchController.php
@@ -6,6 +6,7 @@ namespace App\Controller;
 use App\Entity\User;
 use App\Form\QuickSearchRouteType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\HttpFoundation\Session\Session;
@@ -20,21 +21,24 @@ class QuickSearchController extends AbstractController
      */
     public function index(Request $request)
     {
+        if ($this->get('security.authorization_checker')->isGranted('IS_AUTHENTICATED_FULLY')) {
+            return $this->redirectToRoute('homepage');
+        }
         $routeForm = $this->createForm(QuickSearchRouteType::class);
         $routeForm->handleRequest($request);
 
         if ($routeForm->isSubmitted() && $routeForm->isValid()) {
-            if(session_status() == PHP_SESSION_NONE) {
-                $session = new Session();
-                $session->start();
+            if($this->get('session')->get('user')) {
+                $user = $this->get('session')->get('user');
+                $user->setRoute($routeForm->getData()['route']);
+                $this->get('session')->set('user', $user);
+            } else {
                 $user = new User();
                 $user->setRoute($routeForm->getData()['route']);
                 $user->setRole('client');
-                $session->set('user', $user);
-            } else {
-                $user = $session->get('user');
-                $user->setRoute($routeForm->getData()['route']);
+                $this->get('session')->set('user', $user);
             }
+            return new RedirectResponse($this->generateUrl('quick_match'));
         }
 
         return $this->render('quick_search/index.html.twig', [
